8177dd8efdd462532c718a9af4eb982733dc84fc
local ho3t <local.python@yahoo.com>
2023-07-01 22:44:32 +0330

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from start.start import starter
+
+
+def App():
+    starter()
+
+
+if __name__ == "__main__":
+    App()
---
+++ start/start.py
@@ -0,0 +1,5 @@
+from app.banner.v1.banner import banner
+
+
+def starter():
+    banner()
